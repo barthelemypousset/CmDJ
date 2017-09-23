2ad19175a9b1c4e9c272afb953f20c83a5e4d43e
bpo <barthelemypousset@gmail.com>
2017-09-23 19:27:05 +0200

    First commit

---
+++ cmdj.py
@@ -0,0 +1,10 @@
+import wave
+import numpy as np
+
+source = open("source.dj","r")
+piano = wave.open("sample\piano.wav","r")
+
+#print(type(source))
+
+print(source.read())
+print(Wave_read.getnframes(piano))
